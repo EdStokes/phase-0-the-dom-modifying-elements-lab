f6152e0579b475523aee1098e1c899e69f864b19
Ed Stokes <edstokes96@gmail.com>
2023-03-12 18:10:10 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent="Ed is the champion";
+document.body.append(newHeader);
+
+
+
+
+
+
+
+
+
